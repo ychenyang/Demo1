efce265bf622b17c632606f1fe000922ebef7d60
ycy <ycy896789@163.com>
2018-01-25 13:38:26 +0800

    change1

---
+++ src/demo4J.java
@@ -0,0 +1,8 @@
+public class demo4J {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a+b;
+        System.out.println("sum="+sum);
+    }
+}
